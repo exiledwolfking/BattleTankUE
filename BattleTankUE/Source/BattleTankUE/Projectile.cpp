2952af2bd59716994eac65f8ac9840d6b77d7086
exiledwolfking <kylebwahl@gmail.com>
2019-03-30 20:51:26 -0500

    Added launch blast to projectile

--- BattleTankUE/Source/BattleTankUE/Projectile.cpp
+++ BattleTankUE/Source/BattleTankUE/Projectile.cpp
@@ -9,7 +9,7 @@
 AProjectile::AProjectile()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bCanEverTick = true;
 
 	CollisionMesh = CreateDefaultSubobject<UStaticMeshComponent>(FName("Projectile Collision Mesh"));
 	SetRootComponent(CollisionMesh);
--- BattleTankUE/Source/BattleTankUE/Tank/TankTrackComponent.cpp
+++ BattleTankUE/Source/BattleTankUE/Tank/TankTrackComponent.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Engine/World.h"
 #include "TankTrackComponent.h"
+#include "Engine/World.h"
 
 
 UTankTrackComponent::UTankTrackComponent()
